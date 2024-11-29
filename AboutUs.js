6f268c458feb8e9c6eaf9e1e04c9a572a849109f
shefaa121 <shefa.albeity1@gmail.com>
2024-11-29 18:12:24 +0400

    Add files via upload

---
+++ AboutUs.js
@@ -0,0 +1,13 @@
+// About-US.js
+
+// Prompt user for their name
+const userName = prompt("Hey there! What’s your lovely name?");
+
+// Check if the user entered a name and display the greeting
+if (userName) {
+    // Display a personalized message in a window alert
+    alert(`Hello, ${userName}! At Evonaire, we believe your dreams can come true, and we're so excited to be part of your journey! 💖`);
+} else {
+    // If no name is entered, display a default message in a window alert
+    alert("Hello, beautiful soul! 💖 At Evonaire, we believe in the power of dreams and we're thrilled to have you here! ✨");
+}
